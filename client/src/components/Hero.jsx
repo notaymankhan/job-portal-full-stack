5880e4918891c8187570b01b708df908ee984133
Akha60 <ayman.khan@nike.com>
2025-06-12 11:21:33 +0530

    local ui changes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
 
             <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
                 <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
-                    <p className='font-medium'>Trusted by</p>
+                    <p className='font-medium'>Jobs from</p>
                     <img className='h-6' src={assets.microsoft_logo} alt="" />
                     <img className='h-6' src={assets.walmart_logo} alt="" />
                     <img className='h-6' src={assets.accenture_logo} alt="" />
